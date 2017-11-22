e403e198aca8bb63e1efff79546e45ad31696dc6
Mark Pollack <mpollack@pivotal.io>
2017-11-22 13:42:44 -0500

    Made StreamBuilder a top level class

--- spring-cloud-dataflow-rest-client/src/main/java/org/springframework/cloud/dataflow/rest/client/dsl/Stream.java
+++ spring-cloud-dataflow-rest-client/src/main/java/org/springframework/cloud/dataflow/rest/client/dsl/Stream.java
@@ -1,10 +1,8 @@
 package org.springframework.cloud.dataflow.rest.client.dsl;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.cloud.dataflow.rest.client.DataFlowOperations;
 import org.springframework.cloud.dataflow.rest.resource.StreamDefinitionResource;
@@ -24,7 +22,7 @@ public class Stream {
 
 	private DataFlowOperations client;
 
-	private Stream(String name, List<StreamApplication> applications, String definition,
+	Stream(String name, List<StreamApplication> applications, String definition,
 			DataFlowOperations client) {
 		this.name = name;
 		this.applications = applications;
@@ -101,8 +99,7 @@ public class Stream {
 		}
 
 		/**
-		 * Creates a Stream bypassing the fluent API and just using the provided
-		 * definition
+		 * Creates a Stream bypassing the fluent API and just using the provided definition
 		 * @param definiton the Stream definition to use
 		 * @return A {@link Stream} object
 		 */
@@ -154,71 +151,6 @@ public class Stream {
 		public StreamBuilder create() {
 			return new StreamBuilder(this.name, this.client, this.definition,
 					Collections.emptyList());
-		}
-	}
-
-	public static class StreamBuilder {
-
-		private String name;
-
-		private DataFlowOperations client;
-
-		private String definition;
-
-		private List<StreamApplication> applications = new LinkedList<>();
-
-		private StreamBuilder(String name, DataFlowOperations client, String definition,
-				List<StreamApplication> applications) {
-			this.name = name;
-			this.client = client;
-			this.definition = definition;
-			this.applications = applications;
-			if (StringUtils.isEmpty(definition)) {
-				createStreamDefinition();
-			}
-			this.client.streamOperations().createStream(this.name, this.definition,
-					false);
-		}
-
-		public void destroy() {
-			this.client.streamOperations().destroy(this.name);
-		}
-
-		public Stream deploy(Map<String, String> deploymentProperties) {
-			Map<String, String> resolvedProperties = resolveDeploymentProperties(
-					deploymentProperties);
-			client.streamOperations().deploy(this.name, resolvedProperties);
-			return new Stream(this.name, this.applications, this.definition, this.client);
-		}
-
-		public Stream deploy() {
-			return deploy(null);
-		}
-
-		private void createStreamDefinition() {
-			StringBuilder buffer = new StringBuilder();
-			this.definition = StringUtils.collectionToDelimitedString(applications,
-					" | ");
-		}
-
-		/**
-		 * Concatenates any deployment properties from the apps with a given map used
-		 * during {@link StreamBuilder#deploy(Map)}
-		 * @return
-		 */
-		private Map<String, String> resolveDeploymentProperties(
-				Map<String, String> deploymentProperties) {
-			Map<String, String> properties = new HashMap<>();
-			if (deploymentProperties != null) {
-				properties.putAll(deploymentProperties);
-			}
-			for (StreamApplication app : this.applications) {
-				for (Map.Entry<String, Object> entry : app.getDeploymentProperties()
-						.entrySet()) {
-					properties.put(entry.getKey(), entry.getValue().toString());
-				}
-			}
-			return properties;
 		}
 	}
 
