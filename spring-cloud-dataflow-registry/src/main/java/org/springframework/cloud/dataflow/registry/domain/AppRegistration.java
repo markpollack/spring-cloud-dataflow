07c72fc1a0e43ba194179a172fb590c1d7fb0fc7
Mark Pollack <mpollack@pivotal.io>
2018-01-31 12:04:24 -0500

    Upgrade to Hibernate 5.1.12, use @Lob for URIs in AppRegistration
    Fixes #2020
    Fixes #2018

--- spring-cloud-dataflow-registry/src/main/java/org/springframework/cloud/dataflow/registry/domain/AppRegistration.java
+++ spring-cloud-dataflow-registry/src/main/java/org/springframework/cloud/dataflow/registry/domain/AppRegistration.java
@@ -18,6 +18,7 @@ package org.springframework.cloud.dataflow.registry.domain;
 
 import java.net.URI;
 import javax.persistence.Entity;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 
 import org.springframework.cloud.dataflow.core.ApplicationType;
@@ -53,12 +54,14 @@ public class AppRegistration extends AbstractEntity implements Comparable<AppReg
 	/**
 	 * URI for the app resource.
 	 */
+	@Lob
 	private URI uri;
 
 	/**
 	 * URI for the app metadata or {@literal null} if the app itself should be used as
 	 * metadata source.
 	 */
+	@Lob
 	private URI metadataUri;
 
 	/**
